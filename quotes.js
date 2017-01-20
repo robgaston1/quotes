f093241748e67d82f4297e8e0ca7559d4d32367b
robgaston1 <robcoach0221@gmail.com>
2017-01-20 06:18:32 -0500

    fixing a problem with adding multiple twweet buttons upon refresh

--- quotes.js
+++ quotes.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
             $("#phrase").text(newquote);
             $("#auth").text(data.author);
             var tweet= newquote + "," + data.author;
-            $("#quotebox").append("<a class=\"twitter-share-button\" href=\"https://twitter.com/intent/tweet?text="+tweet+"\"><button class=\"btn btn-primary\" id=\"tweet\"><i class=\"fa fa-twitter\" aria-hidden=\"true\"></i> Tweet</button></a>");
+            $("#tweet").html("<a class=\"twitter-share-button\" href=\"https://twitter.com/intent/tweet?text="+tweet+"\"><button class=\"btn btn-primary\" id=\"tweet\"><i class=\"fa fa-twitter\" aria-hidden=\"true\"></i> Tweet</button></a>");
         });
       }
   $("#refresh").on("click", function() {
     genQuote();
   });
 
-  $("#tweet").on("click", function() {
+  /*$("#tweet").on("click", function() {
     window.open("https://www.twitter.com/share?text=" + newquote + "&url=https://goo.gl/n2xwuo");
-  });
+  });*/
     genQuote();
 });
